e7ef99b8c7ec160288636c089d0a41a6fecc99cf
杨竞颉 <j.yang19@ejm.org>
2019-08-06 10:23:00 +0800

    tweak css and deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 git branch -D gh-pages
 git push origin --delete gh-pages
-git checkout --orphan=gh-pages
+git branch gh-pages
+git checkout gh-pages
 
 elm make src/Main.elm --optimize --output=elm.js
 uglifyjs elm.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output=elm.min.js
 
-git add . -f
+git add index.html
+git add slidelendar.css
+git add elm.min.js -f
+git add web_assets
 
 git commit -m "deploy"
 
